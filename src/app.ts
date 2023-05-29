7bf199217c9e13bd6914ba32b7a6e7c1ec41a2a9
SafiaJotey <safiaahmed1786@gmail.com>
2023-05-30 02:26:30 +0600

    [DONE] setup eslint, prettier, husky, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import cors from 'cors'
-import express, { Application, Request, urlencoded } from 'express'
+import express, { Application, Request, Response, urlencoded } from 'express'
 const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
