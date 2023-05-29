3b6053e285fcdaa36362ba98061582a4ffac2c5e
SafiaJotey <safiaahmed1786@gmail.com>
2023-05-29 13:35:47 +0600

    [DONE]setup mongoose, express, cors,parser and connect database

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import cors from 'cors';
+import express, { Application, Request, Response, urlencoded } from 'express';
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
