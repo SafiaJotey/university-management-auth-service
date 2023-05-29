d809a74e9bf0dc7b4a7cd212ef0617c41562e82a
SafiaJotey <safiaahmed1786@gmail.com>
2023-05-30 00:49:40 +0600

    test husty

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import cors from 'cors';
-import express, { Application, Request, Response, urlencoded } from 'express';
-const app: Application = express();
-app.use(cors());
-app.use(express.json());
-app.use(urlencoded({ extended: true }));
+import cors from 'cors'
+import express, { Application, Request, urlencoded } from 'express'
+const app: Application = express()
+app.use(cors())
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+app.get('/', (req: Request, res: any) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
